69ac448dc17c5587542a004fabc6821f95ed3ac6
Erik Maier <rkmaier@gmail.com>
2018-04-09 19:16:06 +0200

    Update config

--- config/pubgapi.php
+++ config/pubgapi.php
@@ -9,6 +9,6 @@ return [
         'na' => 'pc-na',
     ],
 
-    'access_token' =>'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5MzZkNDI4MC0xOWFkLTAxMzYtZjNjNC01M2VjNGIzZGE3NzkiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzg4MjAzLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YnN0YXRzLTM3NzViMjBmLTg5NTQtNGExZC04MTExLWIyZWJhNjExZmQ2OSIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.wHheIn0nq1nJAn-SKTJ_mrXVbGrHuNbZrs882SEmc14',
+    'access_token' =>'',
 
 ];
